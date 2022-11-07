ce525313453c70719458f95ea1a68acf3c2b2b87
firdavs <firdavsyusupov.pc.uz@gmail.com>
2022-11-07 18:53:29 +0500

    Start Backend

--- MarketPlace/ugc/models.py
+++ MarketPlace/ugc/models.py
@@ -13,3 +13,34 @@ class Task(models.Model):
     class Meta:
         verbose_name = 'Задача'
         verbose_name_plural = 'Задачи'
+
+
+class HeroSection(models.Model):
+    title = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="hero/%Y/%m/%d")
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Hero Section'
+        verbose_name_plural = 'Hero Sections'
+
+
+class Mailing(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d")
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=True )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Рассылка'
+        verbose_name_plural = 'Рассылки'
